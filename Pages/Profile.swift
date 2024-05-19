e6c667794453c8edc37f0de6220051ebf2fd8e4c
Astrian Zheng <happy.wheel6743@astrian.moe>
2024-05-19 16:33:16 +1000

    Brand new design of profile page

--- Pages/Profile.swift
+++ Pages/Profile.swift
@@ -32,8 +32,89 @@ struct Profile: View {
                     }
                     
                     Section {
-                        
+                        LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())], spacing: 12) {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text("64").font(.system(size: 26, design: .rounded)).foregroundStyle(Color.accentColor)
+                                    Text("Age").font(.caption)
+                                }
+                                Spacer()
+                            }
+                            
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text("F").font(.system(size: 26, design: .rounded)).foregroundStyle(Color.accentColor)
+                                    Text("Sex").font(.caption)
+                                }
+                                Spacer()
+                            }
+                            
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text("VIC").font(.system(size: 26, design: .rounded)).foregroundStyle(Color.accentColor)
+                                    Text("Location").font(.caption)
+                                }
+                                Spacer()
+                            }
+                            
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text("231").font(.system(size: 26, design: .rounded)).foregroundStyle(Color.accentColor)
+                                    Text("Days in HCC").font(.caption)
+                                }
+                                Spacer()
+                            }
+                            
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text("1,021").font(.system(size: 26, design: .rounded)).foregroundStyle(Color.accentColor)
+                                    Text("Reuests").font(.caption)
+                                }
+                                Spacer()
+                            }
+                        }
+                    } header: {
+                        Text("Basic information")
                     }
+                        
+                    Section {
+                        HStack {
+                            Text("Weight")
+                            Spacer()
+                            Text("64kg").foregroundStyle(.gray)
+                        }
+                        
+                        HStack {
+                            Text("Height")
+                            Spacer()
+                            Text("162cm").foregroundStyle(.gray)
+                        }
+                        
+                        HStack {
+                            Text("Blood type")
+                            Spacer()
+                            Text("O").foregroundStyle(.gray)
+                        }
+                    } header: {
+                        Text("Body health")
+                    }
+                    
+                    Section {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Lorem ipsum").bold().font(.system(.title3, design: .rounded))
+                            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris congue rhoncus porta. Morbi bibendum dui felis, id semper erat euismod ut. Nam scelerisque, urna et mollis mollis, ipsum tortor ornare mauris, vel aliquet arcu quam porttitor nibh.")
+                        }
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Lorem ipsum").bold().font(.system(.title3, design: .rounded))
+                            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris congue rhoncus porta. Morbi bibendum dui felis, id semper erat euismod ut. Nam scelerisque, urna et mollis mollis, ipsum tortor ornare mauris, vel aliquet arcu quam porttitor nibh.")
+                        }
+                    } header: {
+                        Text("Medical history")
+                    }
+                    
+                    Section{
+                        Text("")
+                    }.listRowBackground(Color.clear)
                 }
                 .onChange(of: self.showEditProfile) {
                     self.showTab = !self.showEditProfile
